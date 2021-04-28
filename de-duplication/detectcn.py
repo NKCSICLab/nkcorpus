38135a7c47161e300aa9a502024700b94dbbec3d
tkinf <1823896192@qq.com>
2021-04-28 12:02:13 +0800

    Merge

--- de-duplication/detectcn.py
+++ de-duplication/detectcn.py
@@ -19,8 +19,6 @@ from langdetect import detect
 import time
 import sys
 
-MIN_DOCUMENT_LENGHT = 128
-
 
 def print_progress(prefix, start_time, num_docs,num_fixed_text,
                    num_non_chinese_docs, chars_non_chinese_docs):
@@ -37,7 +35,6 @@ def print_progress(prefix, start_time, num_docs,num_fixed_text,
 def filter_corpus(filename, out_filename, print_interval=10000):
 
     print(' > filtering {}'.format(filename))
-    lg=open('notzh-cn.txt','w')
     num_docs = 0
     num_written_docs = 0
     num_fixed_text = 0
@@ -50,15 +47,7 @@ def filter_corpus(filename, out_filename, print_interval=10000):
                 try:
                     num_docs += 1
                     myjson = json.loads(line)
-                    # Fix text
-                    #text = ftfy.fix_text(myjson['text'])
-                    #if text != myjson['text']:
-                    #    num_fixed_text += 1
-                    #myjson['text'] = text
-                    # Detect language.
-                    #print(myjson)
-                    if detect(myjson['answer']) != 'zh-cn':
-                        #lg.write('[non-chinese text]', myjson)
+                    if detect(myjson['text']) != 'zh-cn':
                         num_non_chinese_docs += 1
                         chars_non_chinese_docs += len(myjson)
                         continue
@@ -79,8 +68,6 @@ def filter_corpus(filename, out_filename, print_interval=10000):
 
 
 if __name__ == '__main__':
-
-    print('building gpt2 dataset ...')
 
     input_filename = sys.argv[1]
     output_filename = sys.argv[2]
--- de-duplication/find_duplicates.py
+++ de-duplication/find_duplicates.py
@@ -53,39 +53,16 @@ if __name__ == '__main__':
     input = sys.argv[1]
     output = sys.argv[2]
     start_time = time.time()
-    # 读取输入输出的文件名
     hasher = minhash.MinHasher(seeds=50, char_ngram=5, hashbytes=4)
     lshcache = cache.Cache(bands=5, hasher=hasher)
-    lines = []
-    # read in the data file and add the first 100 documents to the LSH cache
     fh = open(input, 'rb')
-    for line in fh:
-        lines.append(line.decode('utf8'))
     fp = open(output, 'w')
-
-    # reset file pointer and do LSH
     fh.seek(0)
     feed = fh
     candidates = candidate_duplicates(feed, char_ngram=5, seeds=50, bands=5, hashbytes=4)
-
-    # go over all the generated candidates comparing their similarities
-    similarities = []
     for ((line_a, docid_a), (line_b, docid_b)) in candidates:
-        doc_a, doc_b = lines[line_a], lines[line_b]
-        shingles_a = shingles(lines[line_a])
-        shingles_b = shingles(lines[line_b])
-
-        jaccard_sim = jaccard(shingles_a, shingles_b)
-        fingerprint_a = set(hasher.fingerprint(doc_a.encode('utf8')))
-        fingerprint_b = set(hasher.fingerprint(doc_b.encode('utf8')))
-        minhash_sim = len(fingerprint_a & fingerprint_b) / len(fingerprint_a | fingerprint_b)
-        similarities.append((docid_a, docid_b, jaccard_sim, minhash_sim))
-
+        fp.write(str(docid_a)+" "+str(docid_b)+'\n')
+    fp.close()
     print('There are {} candidate duplicates in total'.format(len(candidates)))
-    endtime = time.time()
-    print('总共的时间为:', round(endtime - starttime, 2), 'secs')
-    for i in similarities:
-        if i[2] > 0.95:
-            # fp.write(str(i[0])+" "+str(i[1])+" "+str(i[2])+" "+str(i[3])+'\n')
-            # fp.write(str(i[0]+'\n'))
-            fp.write(str(i[1] + '\n'))
+    endtime=time.time()
+    print('总共的时间为:', round(endtime - starttime, 2),'secs')
