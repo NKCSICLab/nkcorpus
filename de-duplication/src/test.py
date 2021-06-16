704b5bd92a7ce2a82e682df4a141d991d5f0aeba
lidongwen <lidongwen19970807@163.com>
2021-06-16 15:04:31 +0800

    delete useless file

--- de-duplication/src/test.py
+++
@@ -1,6 +0,0 @@
-import json
-file = open("../2021-02-03/filtered_clean/31/crawl-data/CC-MAIN-2021-10/segments/1614178347293.1/wet/CC-MAIN-20210224165708-20210224195708-00001.warc.wet.json","r")
-data = json.load(file)
-for i in data.items():
-    print(i)
-    input()
