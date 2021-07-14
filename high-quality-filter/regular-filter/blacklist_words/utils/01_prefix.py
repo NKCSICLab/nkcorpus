c4d5aefd48c2c74370c8e849e3976fd45cc8c3dc
Zhong Zhenyu <nczzy1997@gmail.com>
2021-07-14 10:21:54 +0800

    Update blacklist words process scripts

--- high-quality-filter/regular-filter/blacklist_words/utils/01_prefix.py
+++ high-quality-filter/regular-filter/blacklist_words/utils/01_prefix.py
@@ -1,11 +1,15 @@
-import datetime
-
-today = datetime.date.today().strftime("%y%m%d")
 PREFIX = '色情'
-IN_FILE = f'../data/tmp/{today}_delta.txt'
-OUT_FILE = f'../data/tmp/{today}_prefix.txt'
+IN_FILE = f'../data/tmp/delta.txt'
+OUT_FILE = f'../data/tmp/prefix.txt'
 
 with open(IN_FILE, 'r', encoding='utf-8') as in_file, open(OUT_FILE, 'w', encoding='utf-8') as out_file:
     lines = in_file.readlines()
+    striped = []
     for line in lines:
-        out_file.write(f'{PREFIX} {line}')
+        line = line.strip()
+        if line:
+            striped.append(line)
+    for i in range(l := len(striped)):
+        out_file.write(f'{PREFIX} {striped[i]}')
+        if i != l - 1:
+            out_file.write('\n')
--- high-quality-filter/regular-filter/blacklist_words/utils/02_patch.py
+++ high-quality-filter/regular-filter/blacklist_words/utils/02_patch.py
@@ -1,13 +1,10 @@
-import datetime
-import pathlib
 import shutil
 
-today = datetime.date.today().strftime("%y%m%d")
-IN_FILE = f'../data/tmp/{today}_prefix.txt'
-OUT_FILE = f'../data/tmp/{today}_chs.txt'
+IN_FILE = f'../data/tmp/prefix.txt'
+OUT_FILE = f'../data/tmp/chs.txt'
+LATEST_FILE = f'../data/latest.txt'
 
-latest_file = sorted(list(pathlib.Path('../data').glob('*.txt')))[-1]
-shutil.copy(str(latest_file), OUT_FILE)
+shutil.copy(LATEST_FILE, OUT_FILE)
 with open(OUT_FILE, 'a', encoding='utf-8') as out_file, open(IN_FILE, 'r', encoding='utf8') as in_file:
     content = in_file.read()
     out_file.write('\n')
